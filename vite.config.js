3b691b1276e96f0e951583c94e8ded3c9a6b7dc9
User <user@example.com>
2026-05-09 20:05:11 +0600

    optimize for netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  base: '/blackhole/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'github' ? '/blackhole/' : '/',
+
   plugins: [react(), tailwindcss()],
+
   server: {
     host: true,
     port: 3000
   }
-})
+}))
